662e4982bb7fc1aea6973476225689e0ec0a8bbe
Marc Soda <marc@soda.rip>
2025-07-10 09:55:15 -0400

    merge Rich's stuff

--- react/src/App.tsx
+++ react/src/App.tsx
@@ -1,237 +1,42 @@
-import axios from "axios";
-import { useEffect, useState } from "react";
-import showdown from "showdown";
-import styled, { createGlobalStyle } from "styled-components";
-import Chat, { type ChatMessage, type MessageSender } from "./Chat";
-import ChatInput from "./ChatInput";
+import { Route, Routes } from 'react-router-dom';
 
-const GlobalStyle = createGlobalStyle`
-  *, *::before, *::after {
-    box-sizing: border-box;
-    margin: 0;  
-    padding: 0;
-  }
+import LoginForm from './features/auth/Login';
+import Logout from './features/auth/Logout';
+import MainPage from './features/main/MainPage';
 
-  body {
-    font-family: sans-serif;
-    line-height: 1.6;
-    background-color: #242424;
-  }
-`;
+import ProtectedRoute from './components/common/ProtectedRoute';
+import Sidenav from './components/common/Sidenav';
+import TopNav from './components/common/topnav';
 
-const Header = styled.header`
-  position: fixed;
-  top: 0;
-  left: 0;
-  right: 0;
-  height: 60px;
-  background: #333;
-  color: #fff;
-  display: flex;
-  align-items: center;
-  justify-content: space-between;
-  padding: 0 20px;
-  z-index: 1000;
-`;
+import { Toaster } from "@/components/ui/sonner"
 
-const Logo = styled.div`
-  font-size: 1.25rem;
-  font-weight: bold;
-`;
+import { AuthProvider } from './context/AuthContext';
+import { ThemeProvider } from './context/ThemeContext';
 
-const Nav = styled.nav`
-  ul {
-    list-style: none;
-    display: flex;
-    gap: 20px;
-  }
-`;
+import useHideSidenavOnRoutes from './hooks/useHideSidenavOnRoutes';
 
-const Content = styled.main`
-  padding: 80px 20px 0px; /* offset for header */
-  max-width: 800px;
-  margin: 0 auto;
-`;
+function App() {
+    const showSidenav = useHideSidenavOnRoutes(['/login']);
 
-const Footer = styled.footer`
-  padding: 0px 20px;
-  color: #666;
-  margin-top: 20px;
-`;
-
-const StyledLink = styled.a`
-  color: #fff;
-  text-decoration: none;
-  transition: opacity 0.2s;
-
-  &:hover {
-    opacity: 0.6;
-    color: #fff;
-  }
-`;
-
-const App = () => {
-  const createNewMessage = (message: string, sender: MessageSender) => {
-    return {
-      timestamp: new Date().toLocaleString(),
-      message,
-      sender,
-    } as ChatMessage;
-  };
-
-  const botName = "Shadowbase AI Assistant";
-  const botWelcomeMessage = `Hello! I am your ${botName}. How can I help you today?`;
-  const botWelcome = createNewMessage(botWelcomeMessage, "bot");
-
-  const [conversationId, setConversationId] = useState("");
-  const [isAsking, setIsAsking] = useState(false);
-  const [_botMessages, setBotMessages] = useState([] as string[]);
-  const [_userMessages, setUserMessages] = useState([] as string[]);
-  const [chatHistory, setChatHistory] = useState([] as ChatMessage[]);
-  const [followUpQuestion, setFollowUpQuestion] = useState("");
-
-  useEffect(() => {
-    if (!chatHistory.length) {
-      initializeChat();
-    }
-  }, []);
-
-  const initializeChat = () => {
-    setUserMessages([]);
-    setBotMessages([botWelcomeMessage]);
-    setChatHistory([botWelcome]);
-  };
-
-  const markdownToHtml = (markdown: string) => {
-    const converter = new showdown.Converter();
-    return converter.makeHtml(markdown);
-  };
-
-  const appendMessage = (message: string, sender: MessageSender) => {
-    if (!message.trim()) return false;
-
-    setChatHistory((history) => [
-      ...history,
-      createNewMessage(message, sender),
-    ]);
-
-    if (sender === "bot") {
-      setBotMessages((history) => [...history, message, sender]);
-    } else {
-      setUserMessages((history) => [...history, message, sender]);
-    }
-    return true;
-  };
-
-  const handleApiError = (error: string) => {
-    console.error(error);
-    switch (Math.floor(Math.random() * 3)) {
-      case 0:
-        appendMessage(
-          "I am not feeling well; let me get back to you later.",
-          "bot"
-        );
-        return;
-      case 1:
-        appendMessage("It's been a tough day, can you try again later?", "bot");
-        return;
-      case 2:
-        appendMessage("Hmmm, my brain is functioning at the moment.", "bot");
-        return;
-      default:
-        appendMessage(
-          "I am having some issues over here. Cleanup, aisle 5!",
-          "bot"
-        );
-        return;
-    }
-  };
-
-  const requestConversationId = () =>
-    !conversationId
-      ? axios
-          .get("/api/start-convo/")
-          .then((response) => {
-            const id = response.data["convo_id"];
-            setConversationId(id);
-            return id;
-          })
-          .catch((err) => {
-            handleApiError(`Could not establish conversation. (${err.message})`);
-            return "";
-          })
-      : Promise.resolve(conversationId);
-
-  const handleAskQuestion = async (question: string) => {
-    try {
-      if (
-        question.toLowerCase() === "clear" ||
-        question.toLowerCase() === "clr"
-      ) {
-        initializeChat();
-        return;
-      }
-
-      if (!appendMessage(question, "user")) {
-        return;
-      }
-
-      setIsAsking(true);
-
-      const id = await requestConversationId();
-
-      if (!id) {
-        return;
-      }
-
-      console.log(`${question} (${id})`);
-
-      await axios
-        .post(`/api/send-msg/${id}`, {
-          contents: question,
-        })
-        .then((response) =>
-          appendMessage(markdownToHtml(response.data["answer"]), "bot")
-        )
-        .catch((err) => handleApiError(err.message));
-    } finally {
-      setIsAsking(false);
-    }
-  };
-
-  return (
-    <>
-      <GlobalStyle />
-      <Header>
-        <Logo>{botName}</Logo>
-        <Nav>
-          <ul>
-            <li>
-              <StyledLink href="#home">Home</StyledLink>
-            </li>
-            <li>
-              <StyledLink href="#about">Team</StyledLink>
-            </li>
-          </ul>
-        </Nav>
-      </Header>
-      <Content>
-        <Chat
-          botName={botName}
-          chatHistory={chatHistory}
-          awaitingResponse={isAsking}
-          onFollowupClick={setFollowUpQuestion}
-        />
-        <ChatInput
-          onAskQuestion={handleAskQuestion}
-          newValue={followUpQuestion}
-        />
-      </Content>
-      <Footer>
-        &copy; {new Date().getFullYear()} Gravic, Inc. All rights reserved.
-      </Footer>
-    </>
-  );
-};
+    return (
+        <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
+            <AuthProvider>
+                <Toaster richColors />
+                <div className={`flex ${showSidenav ? 'pt-16' : 'pt-0'}`}>
+                    {showSidenav && <TopNav />}
+                    <div className="flex-1">
+                        <div className="p-0">
+                            <Routes>
+                                <Route path="/login" element={<LoginForm />} />
+                                <Route path="/logout" element={<Logout />} />
+                                <Route path="/" element={<ProtectedRoute><MainPage /></ProtectedRoute>} />
+                            </Routes>
+                        </div>
+                    </div>
+                </div>
+            </AuthProvider>
+        </ThemeProvider>
+    );
+}
 
 export default App;
